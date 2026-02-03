eaed262b72512d92ce13c07257ed79a696c02a57
simba.wang <simba1986.wang@gmail.com>
2026-02-03 12:46:18 +0800

    [ENH] enable rendering optimization in InferencePipelinePointMap

--- sam3d_objects/pipeline/inference_pipeline_pointmap.py
+++ sam3d_objects/pipeline/inference_pipeline_pointmap.py
@@ -447,7 +447,7 @@ class InferencePipelinePointMap(InferencePipeline):
                 Enable_occlusion_check=False,
                 Enable_manual_alignment=False,
                 Enable_shape_ICP=False,
-                Enable_rendering_optimization=False,
+                Enable_rendering_optimization=True,
                 min_size=518,
                 device=self.device,
                 backend=backend,
